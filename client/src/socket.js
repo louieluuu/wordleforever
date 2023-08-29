0542cb7ce1edbe4e19c01ac0be3ce984dcf81807
Louie <yi-jie.lu@hotmail.com>
2023-08-29 12:10:21 -0700

    Fix countdown bugs
    1st bug - when a 3rd user joins in the middle of a countdown,
    the number goes crazy for all players (condition change from >1 to == 2)
    
    2nd bug - if you leave room just as countdown ends, it's possible for
    the Public Room to start with just 1 player (add final check)

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,8 +1,11 @@
 import { io } from "socket.io-client"
 
-// "undefined" means the URL will be computed from the `window.location` object
-// the second ternary option must match the server's index.js
-const URL = process.env.NODE_ENV === "production" ? undefined : "localhost:4000"
+// TODO: ec2 might have to start with https:// or ws:// or wss://, check later
+// TODO: (Someone recommended ws://)
+const URL =
+  process.env.NODE_ENV === "production"
+    ? "ec2-18-236-100-196.us-west-2.compute.amazonaws.com"
+    : "localhost:4000"
 
 export const socket = io(URL, {
   transports: ["websocket"], // required when using Vite
