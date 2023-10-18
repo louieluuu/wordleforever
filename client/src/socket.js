50a7494f4b9ad73806485bb3c960aa17fed9b075
Louie <yi-jie.lu@hotmail.com>
2023-10-17 20:23:47 -0700

    Remove c.log

--- client/src/socket.js
+++ client/src/socket.js
@@ -2,8 +2,6 @@ import { io } from "socket.io-client"
 
 const URL = process.env.NODE_ENV === "production" ? import.meta.env.VITE_EC2_URL : "localhost:3005"
 
-console.log(import.meta.env.VITE_EC2_URL)
-
 export const socket = io(URL, {
   transports: ["websocket"], // "websocket" required when using Vite
   // autoConnect: false,
