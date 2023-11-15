6ab0b30fc90af823eb9f956fd99a96eb8e077006
Thomas Chiu <thomas_chiu@sfu.ca>
2023-11-14 16:09:14 -0800

    Fix backspace and enter functionality

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,7 @@ function App() {
       const updatedBoard = board.map((row, rowIndex) => {
         if (rowIndex === activeRowIndex) {
           return row.map((cell, cellIndex) => {
-            if (cellIndex === activeCellIndex) {
+            if (cellIndex === activeCellIndex && board[activeRowIndex][activeCellIndex] === '') {
               return e.toUpperCase();
             } else {
               return cell;
@@ -38,15 +38,16 @@ function App() {
           return row;
         }
       });
+
+      if (activeCellIndex === updatedBoard[activeRowIndex].length) {
+        return;
+      }
       
       setBoard(updatedBoard);
       
       if (activeCellIndex + 1 >= updatedBoard[activeRowIndex].length) {
         if (activeRowIndex + 1 >= updatedBoard.length) {
           // You may handle end of game logic here
-        } else {
-          setActiveRowIndex(activeRowIndex + 1);
-          setActiveCellIndex(0);
         }
       } else {
         setActiveCellIndex(activeCellIndex + 1);
@@ -56,16 +57,26 @@ function App() {
 
   function handleBackspace() {
     const updatedBoard = [...board];
-      if (activeCellIndex > 0) {
+      if (activeCellIndex === updatedBoard[activeRowIndex].length - 1 && updatedBoard[activeRowIndex][activeCellIndex] != '') {
+        console.log("numba 1")
+        console.log(activeCellIndex)
+        updatedBoard[activeRowIndex][activeCellIndex] = '';
+        setBoard(updatedBoard);
+      }
+      else if (activeCellIndex > 0) {
+        console.log("numba 2")
+        console.log(activeCellIndex)
         updatedBoard[activeRowIndex][activeCellIndex - 1] = '';
         setActiveCellIndex(activeCellIndex - 1);
         setBoard(updatedBoard);
-      } else if (activeRowIndex > 0) {
-        updatedBoard[activeRowIndex - 1][updatedBoard[activeRowIndex - 1].length - 1] = '';
-        setActiveRowIndex(activeRowIndex - 1);
-        setActiveCellIndex(updatedBoard[activeRowIndex - 1].length - 1);
-        setBoard(updatedBoard);
       }
+  }
+
+  function handleEnter() {
+    if (activeRowIndex < board.length && activeCellIndex === board[activeRowIndex].length - 1) {
+      setActiveRowIndex(activeRowIndex + 1)
+      setActiveCellIndex(0)
+    }
   }
 
   return (
