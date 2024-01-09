5f9ab90b86a701d6f2d18139dffb736952480c93
Thomas Chiu <thomas_chiu@sfu.ca>
2024-01-08 16:40:04 -0800

    Fix formatting in NavBar

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import { FaRegCircleQuestion } from 'react-icons/fa6'
 import { MdOutlineLeaderboard } from 'react-icons/md'
 import { FaGear } from 'react-icons/fa6'
@@ -7,46 +7,45 @@ import { FaGear } from 'react-icons/fa6'
 import InfoModal from './InfoModal'
 
 function NavBar({
-  setGameMode,
-  setConnectionMode,
+    setGameMode,
+    setConnectionMode,
 }) {
-  const [showInfoModal, setShowInfoModal] = useState(false)
-  const navigate = useNavigate()
+    const [showInfoModal, setShowInfoModal] = useState(false)
 
-  function openInfoModal() {
-    setShowInfoModal(true)
-  }
+    function openInfoModal() {
+        setShowInfoModal(true)
+    }
 
-  function closeInfoModal() {
-    setShowInfoModal(false)
-  }
+    function closeInfoModal() {
+        setShowInfoModal(false)
+    }
 
-  function handleTitleClick() {
-    setGameMode('Easy')
-    setConnectionMode('offline')
-    // Surely this is not the best fix, or even a fix at all, but without the timeout, some errors appear in console before the page refresh
-    setTimeout(() => {
-      window.location.href = '/'
-    }, 0)
-  }
+    function handleTitleClick() {
+        setGameMode('Easy')
+        setConnectionMode('offline')
+        // Surely this is not the best fix, or even a fix at all, but without the timeout, some errors appear in console before the page refresh
+        setTimeout(() => {
+            window.location.href = '/'
+        }, 0)
+    }
 
-  return (
-    <>
-      <div className='navbar'>
-        <div className='navbar__left'>
-          <Link to='/' onClick={handleTitleClick} className='title'>
-            Wordle Battle
-          </Link>
-        </div>
-        <div className='navbar__right'>
-          <FaRegCircleQuestion className='navbar__info' onClick={openInfoModal} />
-          <MdOutlineLeaderboard />
-          <FaGear />
-        </div>
-        <InfoModal show={showInfoModal} handleClose={closeInfoModal} />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className='navbar'>
+                <div className='navbar__left'>
+                    <Link to='/' onClick={handleTitleClick} className='title'>
+                    Wordle Battle
+                    </Link>
+                </div>
+                <div className='navbar__right'>
+                    <FaRegCircleQuestion className='navbar__info' onClick={openInfoModal} />
+                    <MdOutlineLeaderboard />
+                    <FaGear />
+                </div>
+                <InfoModal show={showInfoModal} handleClose={closeInfoModal} />
+            </div>
+        </>
+    )
 }
 
 export default NavBar
