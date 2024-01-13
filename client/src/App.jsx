fcfd535457c0ecf77fbcc9dd40688c579ae42bbe
Thomas Chiu <thomas_chiu@sfu.ca>
2024-01-12 22:46:05 -0800

    Add setIsSpectating prop for offline mode

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -68,6 +68,7 @@ function App() {
                 connectionMode={connectionMode}
                 isHost={isHost}
                 setIsHost={setIsHost}
+                setIsSpectating={setIsSpectating}
               />
             }
           />
