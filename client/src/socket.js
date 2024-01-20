da7dd26ab18b1e392276da9c708a42f53338abc3
Louie <yi-jie.lu@hotmail.com>
2024-01-20 08:16:55 -0800

    Obliterate outdated contents of main branch

--- client/src/socket.js
+++
@@ -1,8 +0,0 @@
-import { io } from "socket.io-client"
-
-const URL = process.env.NODE_ENV === "production" ? import.meta.env.VITE_EC2_URL : "localhost:3005"
-
-export const socket = io(URL, {
-  transports: ["websocket"], // "websocket" required when using Vite
-  // autoConnect: false,
-})
