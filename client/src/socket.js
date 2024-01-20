d23b17a5a2e57e55d3d27aef712a66711e51f4ae
Louie <yi-jie.lu@hotmail.com>
2024-01-20 12:57:03 -0800

    Change vh to dynamic vh (dvh) for mobile

--- client/src/socket.js
+++ client/src/socket.js
@@ -3,7 +3,7 @@ import { io } from "socket.io-client"
 const SERVER_URL =
   process.env.NODE_ENV === "production"
     ? import.meta.env.VITE_EC2_URL
-    : "http://localhost:3005"
+    : `http://${import.meta.env.VITE_IP_ADDR}:3005`
 
 const socket = io(SERVER_URL, { transports: ["websocket"] })
 
