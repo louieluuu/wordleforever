41409869ba6eb843c6f10bf9eefddfd49f968187
Thomas Chiu <thomas_chiu@sfu.ca>
2024-02-01 14:24:10 -0800

    Prevent private room info from shifting when round timer drops digits (100->99, etc)

--- client/src/components/PrivateRoomInfo.jsx
+++ client/src/components/PrivateRoomInfo.jsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
 
 import { LuClock12 } from "react-icons/lu"
 
@@ -9,6 +9,23 @@ function PrivateRoomInfo({
   roundTimer,
   maxRounds,
 }) {
+  const [hiddenDigits, setHiddenDigits] = useState("")
+
+  // Assuming that the round timer should always be styled for a 3 digit starting value
+  useEffect(() => {
+    // If round timer drops to 2 digits, add a hidden digit, so the text doesn't shift
+    if (roundTimer < 100 && roundTimer > 10) {
+      setHiddenDigits("0")
+    }
+    // If round timer drops to 1 digit, add two hidden digits
+    else if (roundTimer < 10) {
+      setHiddenDigits("00")
+    }
+    // No hidden digits if round timer is 3 digits
+    else if (roundTimer > 100) {
+      setHiddenDigits("")
+    }
+  }, [roundTimer])
   return (
     <>
       {connectionMode === "online-private" && roundCounter !== 0 && (
@@ -17,7 +34,7 @@ function PrivateRoomInfo({
             Round: {roundCounter}/{maxRounds}
           </span>
           <span className="timer">
-            <span className="clock">
+            <span className="clock" style={{ verticalAlign: "middle" }}>
               <LuClock12
                 style={{
                   transform: `rotate(${360 - timerIndex * 90}deg)`,
@@ -26,6 +43,7 @@ function PrivateRoomInfo({
               &nbsp;
             </span>
             {roundTimer}
+            <span className="hidden-digits">{hiddenDigits}</span>
           </span>
         </div>
       )}
