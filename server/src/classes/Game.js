f93ed2abd1fe20858d0066b2fad18200a3f0454b
Louie <yi-jie.lu@hotmail.com>
2024-02-03 13:13:18 -0800

    Change PRIVATE_GAME_ROUND_LIMIT back

--- server/src/classes/Game.js
+++ server/src/classes/Game.js
@@ -22,7 +22,7 @@ const PRIVATE_GAME_TIMER = 120
 const PRIVATE_GAME_SOLVED_TIMER = 45
 const ROUND_BREAK_TIME = 7.5
 
-const PRIVATE_GAME_ROUND_LIMIT = 1
+const PRIVATE_GAME_ROUND_LIMIT = 10
 
 export default class Game {
   constructor() {
