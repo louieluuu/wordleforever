6025b8dc735a64ad27b70a0a7377a32d79407cd1
Louie <yi-jie.lu@hotmail.com>
2024-02-07 11:38:43 -0800

    Change socket.js logic to account for users and guests

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,5 +1,7 @@
 import { io } from "socket.io-client"
 
+import { auth } from "./firebase"
+
 const SERVER_URL =
   process.env.NODE_ENV === "production"
     ? import.meta.env.VITE_EC2_URL
@@ -8,11 +10,29 @@ const SERVER_URL =
 const socket = io(SERVER_URL, { transports: ["websocket"] })
 
 socket.on("connect", () => {
-  console.log("Connected to server")
+  console.log(`Connected to server with ID: ${socket.id}.`)
+
+  // Using the native Firebase method instead of the react-firebase-hook.
+  // Hooks can only be used in function components, which would require
+  // turning socket.js into an empty-render Socket component.
+  auth.onAuthStateChanged((user) => {
+    const userId = user ? user.uid : null
+
+    // Attaching custom property "userId" to socket.
+    if (userId) {
+      socket.userId = userId
+      console.log(`Auth successful with userId: ${userId}`)
+    } else {
+      socket.userId = null
+      console.log("No auth.")
+    }
+
+    socket.emit("newConnection")
+  })
 })
 
 socket.on("disconnect", () => {
-  console.log("Disconnected from server")
+  console.log("Disconnected from server.")
 })
 
 export default socket
