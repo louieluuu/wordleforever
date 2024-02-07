d271602a1261aa4191d29e5d81002de93b6674c6
Louie <yi-jie.lu@hotmail.com>
2024-02-07 11:26:37 -0800

    Add cleanupDatabase function for ease of MongoDB user testing

--- server/src/database/db.js
+++ server/src/database/db.js
@@ -1,8 +1,19 @@
 import mongoose from "mongoose"
 import dotenv from "dotenv"
+import User from "./User.js"
 
 // import.meta.env doesn't work in Node.js
 dotenv.config()
+
+async function cleanupDatabase() {
+  try {
+    await User.deleteMany({})
+    console.log("Database wiped clean.")
+  } catch (error) {
+    console.error(`Error cleaning up database: ${error.message}`)
+    throw error
+  }
+}
 
 const MONGO_DB_USER = process.env.MONGO_DB_USER
 const MONGO_DB_PW = process.env.MONGO_DB_PW
@@ -20,6 +31,7 @@ const db = mongoose.connection
 db.on("error", console.error.bind(console, "MongoDB connection error:"))
 db.once("open", async () => {
   console.log("Connected to MongoDB")
+  // await cleanupDatabase()
 })
 
 export { mongoose }
